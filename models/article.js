c8f8a04e7a865b86157b2b2c75d9939c2cec0f21
Osiris <enigma.osiris@gmail.com>
2020-04-15 13:14:36 -0500

    Add files via upload

--- models/article.js
+++ models/article.js
@@ -4,11 +4,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ArticleSchema = Schema({
-    title:String,
-    content:String,
-    date:{type:Date, default:Date.now},
-    image:String
+    title: String,
+    content: String,
+    date:{type: Date, default: Date.now},
+    image: String
 });
 
 module.exports = mongoose.model('Article',ArticleSchema);
-// articles --> save documents with this type in the collection
+//articles -> guarda documentos de este tipo y con esta estructura dentro de la coleccion
