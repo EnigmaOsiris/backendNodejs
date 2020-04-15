55cbd9a1b661d037c6b4e97eb68b04f96b505b5a
Osiris <enigma.osiris@gmail.com>
2020-04-15 13:15:24 -0500

    Add files via upload

--- routes/article.js
+++ routes/article.js
@@ -4,30 +4,24 @@ var express = require('express');
 var ArticleController = require('../controllers/article');
 
 var router = express.Router();
+var multipart = require ('connect-multiparty');
+var md_upload = multipart({uploadDir:'./upload/articles'});
 
-//cargar archivos
-var multipart = require('connect-multiparty');
-var md_upload = multipart({uploadDir: './upload/articles'});
-
-
-router.post('/datos-curso',ArticleController.datosCurso);
-router.get('/test-controller',ArticleController.test);
-
-//routes for articles
-//post
-router.post('/save',ArticleController.save);
-router.post('/upload-image/:id',md_upload,ArticleController.uploadImage);
-
-//gets
+//rutas prueba
+router.post('datosCurso',ArticleController.datosCurso);
+router.get('/test',ArticleController.test);
+//util routes
+router.post('/save', ArticleController.save);
+router.post('/upload_imagen/:id?',md_upload,ArticleController.upload);
+//get
+router.get('/articles/:last?',ArticleController.getAllArticles);
 router.get('/article/:id',ArticleController.getArticle);
-router.get('/articles/:last?',ArticleController.getArticles);
-router.get('/get-image/:image',ArticleController.getImage);
+router.get('/get_image/:image',ArticleController.getImage);
 router.get('/search/:search',ArticleController.search);
 
-//puts
-router.put('/article/:id',ArticleController.updateArticle);
+//put
+router.put('/article/:id',ArticleController.getArticle);
 
 //delete
-router.delete('/article/:id',ArticleController.deleteArticle);
-
-module.exports = router;
+router.delete('/delete/:id',ArticleController.delete);
+module.exports= router;
